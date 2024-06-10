10ee3ef7082f64404cb6b70af99986c5b887228f
W0NYV <w0nyv.707@gmail.com>
2024-06-11 00:44:08 +0900

    v1 完成

--- Assets/Scripts/CosineGradientEffector.cs
+++ Assets/Scripts/CosineGradientEffector.cs
@@ -8,8 +8,90 @@ public class CosineGradientEffector : MonoBehaviour
     [SerializeField] RenderTexture _out;
     [SerializeField] Material _mat;
 
+    private Vector4 _phase;
+    private Vector4 _amplitude;
+    private Vector4 _freq;
+    private Vector4 _offset;
+
     private void Update()
     {
         Graphics.Blit(_in, _out, _mat);
+    }
+
+    public void SetApplyValue(float value)
+    {
+        _mat.SetFloat("_ApplyValue", value);
+    }
+
+    public void SetPhaseR(float value)
+    {
+        _phase = new Vector4(value, _phase.y, _phase.z, _phase.w);
+        _mat.SetVector("_Phase", _phase);
+    }
+
+    public void SetPhaseG(float value)
+    {
+        _phase = new Vector4(_phase.x, value, _phase.z, _phase.w);
+        _mat.SetVector("_Phase", _phase);
+    }
+
+    public void SetPhaseB(float value)
+    {
+        _phase = new Vector4(_phase.x, _phase.y, value, _phase.w);
+        _mat.SetVector("_Phase", _phase);
+    }
+
+    public void SetAmpR(float value)
+    {
+        _amplitude = new Vector4(value, _amplitude.y, _amplitude.z, _amplitude.w);
+        _mat.SetVector("_Amplitude", _amplitude);
+    }
+
+    public void SetAmpG(float value)
+    {
+        _amplitude = new Vector4(_amplitude.x, value, _amplitude.z, _amplitude.w);
+        _mat.SetVector("_Amplitude", _amplitude);
+    }
+
+    public void SetAmpB(float value)
+    {
+        _amplitude = new Vector4(_amplitude.x, _amplitude.y, value, _amplitude.w);
+        _mat.SetVector("_Amplitude", _amplitude);
+    }
+
+    public void SetFreqR(float value)
+    {
+        _freq = new Vector4(value, _freq.y, _freq.z, _freq.w);
+        _mat.SetVector("_Frequency", _freq);
+    }
+
+    public void SetFreqG(float value)
+    {
+        _freq = new Vector4(_freq.x, value, _freq.z, _freq.w);
+        _mat.SetVector("_Frequency", _freq);
+    }
+
+    public void SetFreqB(float value)
+    {
+        _freq = new Vector4(_freq.x, _freq.y, value, _freq.w);
+        _mat.SetVector("_Frequency", _freq);
+    }
+
+    public void SetOffsetR(float value)
+    {
+        _offset = new Vector4(value, _offset.y, _offset.z, _offset.w);
+        _mat.SetVector("_Offset", _offset);
+    }
+
+    public void SetOffsetG(float value)
+    {
+        _offset = new Vector4(_offset.x, value, _offset.z, _offset.w);
+        _mat.SetVector("_Offset", _offset);
+    }
+
+    public void SetOffsetB(float value)
+    {
+        _offset = new Vector4(_offset.x, _offset.y, value, _offset.w);
+        _mat.SetVector("_Offset", _offset);
     }
 }
--- Assets/Shaders/CosineGradient.shader
+++ Assets/Shaders/CosineGradient.shader
@@ -3,6 +3,13 @@ Shader "Hidden/CosineGradient"
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
+
+        _Phase ("Phase", Vector) = (0, 0, 0, 0)
+        _Amplitude ("Amplitude", Vector) = (1, 1, 1, 1)
+        _Frequency ("Frequency", Vector) = (1, 1, 1, 1)
+        _Offset ("Offset", Vector) = (0, 0, 0, 0)
+
+        _ApplyValue ("ApplyValue", float) = 0.0
     }
     SubShader
     {
@@ -38,13 +45,41 @@ Shader "Hidden/CosineGradient"
             }
 
             sampler2D _MainTex;
+            float4 _Phase;
+            float4 _Amplitude;
+            float4 _Frequency;
+            float4 _Offset;
+            float _ApplyValue;
+
+            float4 cosine_gradient(float x, float4 phase, float4 amp, float4 freq, float4 offset)
+            {
+                float TAU = 2. * 3.14159265;
+                phase *= TAU;
+                x *= TAU;
+
+                return float4(
+                    offset.r + amp.r * 0.5 * cos(x * freq.r + phase.r + _Time.y) + 0.5,
+                    offset.g + amp.g * 0.5 * cos(x * freq.g + phase.g + _Time.y) + 0.5,
+                    offset.b + amp.b * 0.5 * cos(x * freq.b + phase.b + _Time.y) + 0.5,
+                    offset.a + amp.a * 0.5 * cos(x * freq.a + phase.a + _Time.y) + 0.5
+                );
+            }
 
             fixed4 frag (v2f i) : SV_Target
             {
                 fixed4 col = tex2D(_MainTex, i.uv);
-                // just invert the colors
-                col.rgb = 1 - col.rgb;
-                return col;
+                
+                float brightness = max(col.r, max(col.g, col.b));
+
+                float4 cos = float4(0, 0, 0, 1);
+
+                if (brightness != 0.0)
+                {
+                    cos = cosine_gradient(brightness, _Phase, _Amplitude, _Frequency, _Offset);
+                    cos = float4(cos.rgb, 1.0);
+                }
+                
+                return lerp(col, cos, _ApplyValue);
             }
             ENDCG
         }
